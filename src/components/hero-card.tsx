2a82565f5359c8d37a60f8930cd1465236d30bbf
Bryan Huynh <bryan.huynh1@ucalgary.ca>
2025-08-22 04:16:36 -0600

    first commit

---
+++ src/components/hero-card.tsx
@@ -0,0 +1,12 @@
+export const HeroCard = () => {
+	return (
+		<>
+			<div className="justify-items-center">
+				<img src="src\assets\PFP.png" alt="Profile Picture" />
+                <h1> Hello! I am Bryan.</h1>
+                <h1 className="text-gray-400"> A full stack developer</h1>
+            </div>
+
+		</>
+	);
+};
