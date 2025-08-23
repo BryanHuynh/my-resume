91a8eddab5821a49162a81f14e55a44944727874
Bryan Huynh <bryan.huynh1@ucalgary.ca>
2025-08-23 10:08:21 -0600

    added open for work banner to hero card

--- src/components/hero-card.tsx
+++ src/components/hero-card.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
 import pfp from "../assets/PFP.png";
+import openForWork from "../assets/open-for-work.png";
+import { OpenForWorkFetch } from "../services/open-for-work-fetch";
 
 export const HeroCard = () => {
+	const [openForWorkFlag, setOpenForWorkFlag] = useState(false);
+
+	useEffect(() => {
+		async function fetchOpenForWork() {
+			const flag = await OpenForWorkFetch.fetch();
+			setOpenForWorkFlag(flag);
+		}
+		fetchOpenForWork();
+	}, []);
+
 	return (
 		<>
 			<div className="items-center mb-10 flex flex-col">
-				<img src={pfp} className="w-64 md:w-64" alt="Profile Picture" />
+				<div className="relative">
+					<img src={pfp} className="w-32 md:w-64" alt="Profile Picture" />
+					<img
+						style={{ display: openForWorkFlag ? "block" : "none" }}
+						src={openForWork}
+						className="w-32 h-full md:w-64 absolute top-0 right-0"
+						alt="Profile Picture"
+					/>
+				</div>
 				<span className="text-3xl md:text-6xl">Hello! I am Bryan.</span>
 				<span className="text-2xl md:text-6xl text-gray-400"> A full stack developer</span>
 			</div>
