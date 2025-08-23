91019469ed75b1f5873a2eac70955ae9b899f365
Bryan Huynh <bryan.huynh1@ucalgary.ca>
2025-08-22 19:51:18 -0600

    created github pages deployment

--- src/components/hero-card.tsx
+++ src/components/hero-card.tsx
@@ -1,8 +1,10 @@
+import pfp from "../assets/PFP.png";
+
 export const HeroCard = () => {
 	return (
 		<>
 			<div className="justify-items-center mb-10">
-				<img src="src\assets\PFP.png" alt="Profile Picture" />
+				<img src={pfp} alt="Profile Picture" />
                 <h1> Hello! I am Bryan.</h1>
                 <h1 className="text-gray-400"> A full stack developer</h1>
             </div>
