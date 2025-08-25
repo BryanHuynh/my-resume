8e54528aa6d7e7e34b191d8ce40c5d7b69ff998e
Bryan Huynh <bryan.huynh1@ucalgary.ca>
2025-08-25 05:35:16 -0600

    forwarded hosting so its easy to use with emulator

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  base: "/my-resume",
-})
+	server: {
+		host: true,
+		port: 3000,
+	},
+	plugins: [tailwindcss()],
+	base: "/my-resume",
+});
