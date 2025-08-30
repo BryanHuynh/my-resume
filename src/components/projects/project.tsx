95b3460f3f86716d0f72246e44f3a246ee1e9b61
Bryan Huynh <bryan.huynh1@ucalgary.ca>
2025-08-29 22:40:34 -0600

    added use memo so labels dont change color when you open desc

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -1,12 +1,30 @@
 import type { ProjectType } from "../../services/projects-services";
 import { SkillLabel } from "../skills-banner/skill-label";
 import { RandomColorGenerator } from "../../utils/random-color-generator";
+import { useMemo, useState } from "react";
 
 type ProjectProps = {
 	project: ProjectType;
 };
 
 export const Project = ({ project }: ProjectProps) => {
+	const [expandLongDesc, setExpandLongDesc] = useState<boolean>(false);
+
+	const skillsDom = useMemo(() => {
+		return (
+			<div className="flex flex-wrap mt-1">
+				{project.skills.map((skill, i) => (
+					<SkillLabel
+						key={i}
+						skill={skill}
+						color={RandomColorGenerator.generate()}
+						size={"sm"}
+					/>
+				))}
+			</div>
+		);
+	}, [project.skills]);
+
 	return (
 		<div className="flex flex-col p-3">
 			<a
@@ -27,32 +45,36 @@ export const Project = ({ project }: ProjectProps) => {
 				/>
 			)}
 
+			{project.skills?.length ? skillsDom : null}
+
 			{project.short_description && (
 				<p className="text-sm md:text-base text-gray-700">{project.short_description}</p>
 			)}
+			{project.long_description && expandLongDesc && (
+				<p className="text-sm md:text-base text-gray-700">{project.long_description}</p>
+			)}
+			<div className="flex flex-row justify-center gap-5">
+				{project.long_description && (
+					<div className="mt-2">
+						<button
+							onClick={() => setExpandLongDesc(!expandLongDesc)}
+							className="text-gray-60 !bg-transparent border-none p-0 hover:underline relative !cursor-pointer"
+						>
+							{expandLongDesc ? "Show less" : "Show more"}
+						</button>
+					</div>
+				)}
 
-			{project.skills?.length ? (
-				<div className="flex flex-wrap mt-1">
-					{project.skills.map((skill, i) => (
-						<SkillLabel
-							key={i}
-							skill={skill}
-							color={RandomColorGenerator.generate()}
-							size={"sm"}
-						/>
-					))}
+				<div className="mt-2">
+					<a
+						href={project.url}
+						target="_blank"
+						rel="noreferrer"
+						className="text-blue-600 hover:text-blue-800 text-sm md:text-base"
+					>
+						View Project →
+					</a>
 				</div>
-			) : null}
-
-			<div className="mt-2">
-				<a
-					href={project.url}
-					target="_blank"
-					rel="noreferrer"
-					className="text-blue-600 hover:text-blue-800 text-sm md:text-base"
-				>
-					View Project →
-				</a>
 			</div>
 		</div>
 	);
