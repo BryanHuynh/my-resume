1a2e09bf4d306c676b4d0d30c9b2ebe0b9085528
Bryan Huynh <bryan.huynh1@ucalgary.ca>
2025-08-29 22:40:47 -0600

    added a short and long desc for testing

--- src/services/projects-services.ts
+++ src/services/projects-services.ts
@@ -23,6 +23,8 @@ export const ProjectService = {
 					"https://cdn.dribbble.com/userupload/13167768/file/original-08e29755d8f12fdb9ef53d5b88bfeef0.jpg?resize=752x&vertical=center",
 				skills: ["typescript", "postgresSQL", "discord.js", "node.js", "github actions"],
 				url: "https://github.com/BryanHuynh/LeetCodeDiscordBot",
+				short_description: "A Discord bot that showcases your accepted LeetCode solutions directly in your server.",
+				long_description: "A bot designed specifically for competitive programming groups. It allows discord member to link their public leetcode account to their discord account. Upon Successfully completing a problem on Leetcode profile, it will notify the user if they would like to share said solution to the reset of the server. Upon Sharing it will create a public thread on an assigned channel to allow for collaboration and discussion about their solution" 
 			},
 			{
 				name: "Leetcode Discord Bot",
