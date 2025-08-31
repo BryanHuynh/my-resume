ba443293de4e793a8cbd1d7eac53af9a2624fdf4
Bryan Huynh <bryan.huynh1@ucalgary.ca>
2025-08-30 20:06:27 -0600

    added email to hero card

---
+++ src/components/email/email-element.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { ClipboardDocumentIcon } from "@heroicons/react/24/solid";
+import { EmailService } from "../../services/email-service";
+
+export const EmailElement = () => {
+	const [copied, setCopied] = useState(false);
+	const [email, setEmail] = useState<string>();
+
+	useEffect(() => {
+		async function fetchEmail() {
+			setEmail(await EmailService.fetch());
+		}
+		fetchEmail();
+	}, []);
+
+	const handleCopy = async (text: string) => {
+		try {
+			await navigator.clipboard.writeText(text);
+			setCopied(true);
+
+			setTimeout(() => setCopied(false), 2000);
+		} catch (err) {
+			console.error("Failed to copy: ", err);
+		}
+	};
+
+	return (
+		<div>
+			{email && (
+				<span
+					onClick={() => handleCopy("Bryan.Huynh42@gmail.com")}
+					className="mt-3 text-md md:text-lg text-gray-400 flex flex-row gap-1 items-center cursor-pointer"
+				>
+					Bryan.Huynh42@gmail.com
+					<ClipboardDocumentIcon className="w-5 h-5" />
+				</span>
+			)}
+			{copied && <div className="mt-2 text-green-600 text-sm">✅ Copied to clipboard!</div>}
+		</div>
+	);
+};
---
+++ src/services/email-service.ts
@@ -0,0 +1,5 @@
+export const EmailService = {
+    async fetch (): Promise<string> {
+        return Promise.resolve('Bryan.Huynh42@gmail.com')
+    }
+}
